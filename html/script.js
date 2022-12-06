110b5868267c856eb506d1d154b890c4b3f978f3
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-12-06 15:57:13 +0100

    cleanup(js): syntax

--- html/script.js
+++ html/script.js
@@ -1,8 +1,5 @@
-
-
 const CameraApp = new Vue({
     el: "#camcontainer",
-
     data: {
         camerasOpen: false,
         cameraLabel: ":)",
@@ -11,15 +8,14 @@ const CameraApp = new Vue({
         dateLabel: "04/09/1999",
         timeLabel: "16:27:49",
     },
-
     methods: {
         OpenCameras(label, connected, cameraId, time) {
             var today = new Date();
-            var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
-            var formatTime = "00:" + time
+            var date = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
+            var formatTime = "00:" + time;
 
             this.camerasOpen = true;
-            this.ipLabel = "145.101.0."+cameraId;
+            this.ipLabel = "145.101.0." + cameraId;
             if (connected) {
                 $("#blockscreen").css("display", "none");
                 this.cameraLabel = label;
@@ -31,7 +27,7 @@ const CameraApp = new Vue({
                 $("#connectedlabel").addClass("connect");
             } else {
                 $("#blockscreen").css("display", "block");
-                this.cameraLabel = "ERROR #400: BAD REQUEST"
+                this.cameraLabel = "ERROR #400: BAD REQUEST";
                 this.connectLabel = "CONNECTION FAILED";
                 this.dateLabel = "ERROR";
                 this.timeLabel = "ERROR";
@@ -39,7 +35,6 @@ const CameraApp = new Vue({
                 $("#connectedlabel").removeClass("connect");
                 $("#connectedlabel").addClass("disconnect");
             }
-            
         },
 
         CloseCameras() {
@@ -52,26 +47,25 @@ const CameraApp = new Vue({
         },
 
         UpdateCameraTime(time) {
-            var formatTime = "00:" + time
+            var formatTime = "00:" + time;
             this.timeLabel = formatTime;
         },
     }
 });
 
-HeliCam = {}
-Databank = {}
-Fingerprint = {}
+HeliCam = {};
+Fingerprint = {};
 
-HeliCam.Open = function(data) {
+HeliCam.Open = () => {
     $("#helicontainer").css("display", "block");
     $(".scanBar").css("height", "0%");
 }
 
-HeliCam.UpdateScan = function(data) {
-    $(".scanBar").css("height", data.scanvalue +"%");
+HeliCam.UpdateScan = (data) => {
+    $(".scanBar").css("height", data.scanvalue + "%");
 }
 
-HeliCam.UpdateVehicleInfo = function(data) {
+HeliCam.UpdateVehicleInfo = (data) => {
     $(".vehicleinfo").css("display", "block");
     $(".scanBar").css("height", "100%");
     $(".heli-model").find("p").html("MODEL: " + data.model);
@@ -80,53 +74,37 @@ HeliCam.UpdateVehicleInfo = function(data) {
     $(".heli-speed").find("p").html(data.speed + " KM/U");
 }
 
-HeliCam.DisableVehicleInfo = function() {
+HeliCam.DisableVehicleInfo = () => {
     $(".vehicleinfo").css("display", "none");
 }
 
-HeliCam.Close = function() {
+HeliCam.Close = () => {
     $("#helicontainer").css("display", "none");
     $(".vehicleinfo").css("display", "none");
     $(".scanBar").css("height", "0%");
 }
 
-Databank.Open = function() {
-    $(".databank-container").css("display", "block").css("user-select", "none");
-    $(".databank-container iframe").css("display", "block");
-    $(".tablet-frame").css("display", "block").css("user-select", "none");
-    $(".databank-bg").css("display", "block");
-}
-
-Databank.Close = function() {
-    $(".databank-container iframe").css("display", "none");
-    $(".databank-container").css("display", "none");
-    $(".tablet-frame").css("display", "none");
-    $(".databank-bg").css("display", "none");
-    $.post("https://qb-policejob/closeDatabank", JSON.stringify({}));
-}
-
-Fingerprint.Open = function() {
+Fingerprint.Open = () => {
     $(".fingerprint-container").fadeIn(150);
     $(".fingerprint-id").html("Fingerprint ID<p>No result</p>");
 }
 
-Fingerprint.Close = function() {
+Fingerprint.Close = () => {
     $(".fingerprint-container").fadeOut(150);
     $.post('https://qb-policejob/closeFingerprint');
 }
 
-Fingerprint.Update = function(data) {
-    $(".fingerprint-id").html("Fingerprint ID<p>"+data.fingerprintId+"</p>");
+Fingerprint.Update = (data) => {
+    $(".fingerprint-id").html("Fingerprint ID<p>" + data.fingerprintId + "</p>");
 }
 
-$(document).on('click', '.take-fingerprint', function(){
+$(document).on('click', '.take-fingerprint', () => {
     $.post('https://qb-policejob/doFingerScan');
 })
 
 document.onreadystatechange = () => {
     if (document.readyState === "complete") {
-        window.addEventListener('message', function(event) {
-
+        window.addEventListener('message', (event) => {
             if (event.data.type == "enablecam") {
                 CameraApp.OpenCameras(event.data.label, event.data.connected, event.data.id, event.data.time);
             } else if (event.data.type == "disablecam") {
@@ -136,7 +114,7 @@ document.onreadystatechange = () => {
             } else if (event.data.type == "updatecamtime") {
                 CameraApp.UpdateCameraTime(event.data.time);
             } else if (event.data.type == "heliopen") {
-                HeliCam.Open(event.data);
+                HeliCam.Open();
             } else if (event.data.type == "heliclose") {
                 HeliCam.Close();
             } else if (event.data.type == "heliscan") {
@@ -145,10 +123,6 @@ document.onreadystatechange = () => {
                 HeliCam.UpdateVehicleInfo(event.data);
             } else if (event.data.type == "disablescan") {
                 HeliCam.DisableVehicleInfo();
-            } else if (event.data.type == "databank") {
-                Databank.Open();
-            } else if (event.data.type == "closedatabank") {
-                Databank.Close();
             } else if (event.data.type == "fingerprintOpen") {
                 Fingerprint.Open();
             } else if (event.data.type == "fingerprintClose") {
@@ -160,10 +134,9 @@ document.onreadystatechange = () => {
     };
 };
 
-$(document).on('keydown', function() {
-    switch(event.keyCode) {
+$(document).on('keydown', (event) => {
+    switch(event.which) {
         case 27: // ESC
-            Databank.Close();
             Fingerprint.Close();
             break;
     }
