8f35acd13d073a5dd3dc9663c97e7e3dd3c67f1f
Joe Szymkowicz <joe.szymkowicz+githubfivem@gmail.com>
2022-12-07 22:56:07 -0600

    fix: use ox_lib zones, revert zone "in" params

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -13,9 +13,6 @@ shared_scripts {
 }
 
 client_scripts {
-	'@PolyZone/client.lua',
-	'@PolyZone/BoxZone.lua',
-	'@PolyZone/ComboZone.lua',
 	'client/main.lua',
 	'client/camera.lua',
 	'client/interactions.lua',
