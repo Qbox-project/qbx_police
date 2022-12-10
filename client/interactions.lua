cd36327d49513909100793a5e60b96c880c4cd83
HebiKotei <c1yvan@outlook.com>
2022-12-10 20:06:01 +0000

    (ref) Abstraction of PR #9

--- client/interactions.lua
+++ client/interactions.lua
@@ -42,65 +42,75 @@ local function GetCuffedAnimation(playerId)
     Wait(2500)
 end
 
+local function EscortActions()
+    DisableAllControlActions(0)
+    EnableControlAction(0, 1, true)
+    EnableControlAction(0, 2, true)
+    EnableControlAction(0, 245, true)
+    EnableControlAction(0, 38, true)
+    EnableControlAction(0, 322, true)
+    EnableControlAction(0, 249, true)
+    EnableControlAction(0, 46, true)
+end
+
+local function HandcuffActions()
+    DisableControlAction(0, 24, true) -- Attack
+    DisableControlAction(0, 257, true) -- Attack 2
+    DisableControlAction(0, 25, true) -- Aim
+    DisableControlAction(0, 263, true) -- Melee Attack 1
+
+    DisableControlAction(0, 45, true) -- Reload
+    DisableControlAction(0, 22, true) -- Jump
+    DisableControlAction(0, 44, true) -- Cover
+    DisableControlAction(0, 37, true) -- Select Weapon
+    DisableControlAction(0, 23, true) -- Also 'enter'?
+
+    DisableControlAction(0, 288, true) -- Disable phone
+    DisableControlAction(0, 289, true) -- Inventory
+    DisableControlAction(0, 170, true) -- Animations
+    DisableControlAction(0, 167, true) -- Job
+
+    DisableControlAction(0, 26, true) -- Disable looking behind
+    DisableControlAction(0, 73, true) -- Disable clearing animation
+    DisableControlAction(2, 199, true) -- Disable pause screen
+
+    DisableControlAction(0, 59, true) -- Disable steering in vehicle
+    DisableControlAction(0, 71, true) -- Disable driving forward in vehicle
+    DisableControlAction(0, 72, true) -- Disable reversing in vehicle
+
+    DisableControlAction(2, 36, true) -- Disable going stealth
+
+    DisableControlAction(0, 264, true) -- Disable melee
+    DisableControlAction(0, 257, true) -- Disable melee
+    DisableControlAction(0, 140, true) -- Disable melee
+    DisableControlAction(0, 141, true) -- Disable melee
+    DisableControlAction(0, 142, true) -- Disable melee
+    DisableControlAction(0, 143, true) -- Disable melee
+    DisableControlAction(0, 75, true)  -- Disable exit vehicle
+    DisableControlAction(27, 75, true) -- Disable exit vehicle
+    EnableControlAction(0, 249, true) -- Added for talking while cuffed
+    EnableControlAction(0, 46, true)  -- Added for talking while cuffed
+end
+
 local function HandcuffedEscorted()
     local sleep = 1000
+    local anim = {{dict = "mp_arresting", anim = "idle"}, {dict = "mp_arrest_paired", anim = "crook_p2_back_right"}}
+    
     if not IsLoggedIn then return sleep end
     if isEscorted then
         sleep = 0
-        DisableAllControlActions(0)
-        EnableControlAction(0, 1, true)
-        EnableControlAction(0, 2, true)
-        EnableControlAction(0, 245, true)
-        EnableControlAction(0, 38, true)
-        EnableControlAction(0, 322, true)
-        EnableControlAction(0, 249, true)
-        EnableControlAction(0, 46, true)
+        EscortActions()
     end
-
-    if PlayerData.metadata.ishandcuffed then
-        sleep = 0
-        DisableControlAction(0, 24, true) -- Attack
-        DisableControlAction(0, 257, true) -- Attack 2
-        DisableControlAction(0, 25, true) -- Aim
-        DisableControlAction(0, 263, true) -- Melee Attack 1
-
-        DisableControlAction(0, 45, true) -- Reload
-        DisableControlAction(0, 22, true) -- Jump
-        DisableControlAction(0, 44, true) -- Cover
-        DisableControlAction(0, 37, true) -- Select Weapon
-        DisableControlAction(0, 23, true) -- Also 'enter'?
-
-        DisableControlAction(0, 288, true) -- Disable phone
-        DisableControlAction(0, 289, true) -- Inventory
-        DisableControlAction(0, 170, true) -- Animations
-        DisableControlAction(0, 167, true) -- Job
-
-        DisableControlAction(0, 26, true) -- Disable looking behind
-        DisableControlAction(0, 73, true) -- Disable clearing animation
-        DisableControlAction(2, 199, true) -- Disable pause screen
-
-        DisableControlAction(0, 59, true) -- Disable steering in vehicle
-        DisableControlAction(0, 71, true) -- Disable driving forward in vehicle
-        DisableControlAction(0, 72, true) -- Disable reversing in vehicle
-
-        DisableControlAction(2, 36, true) -- Disable going stealth
-
-        DisableControlAction(0, 264, true) -- Disable melee
-        DisableControlAction(0, 257, true) -- Disable melee
-        DisableControlAction(0, 140, true) -- Disable melee
-        DisableControlAction(0, 141, true) -- Disable melee
-        DisableControlAction(0, 142, true) -- Disable melee
-        DisableControlAction(0, 143, true) -- Disable melee
-        DisableControlAction(0, 75, true)  -- Disable exit vehicle
-        DisableControlAction(27, 75, true) -- Disable exit vehicle
-        EnableControlAction(0, 249, true) -- Added for talking while cuffed
-        EnableControlAction(0, 46, true)  -- Added for talking while cuffed
-
-        if (not IsEntityPlayingAnim(cache.ped, "mp_arresting", "idle", 3) and not IsEntityPlayingAnim(cache.ped, "mp_arrest_paired", "crook_p2_back_right", 3)) and not PlayerData.metadata.isdead then
-            lib.requestAnimDict("mp_arresting")
-            TaskPlayAnim(cache.ped, "mp_arresting", "idle", 8.0, -8, -1, cuffType, 0, false, false, false)
-        end
+    if not PlayerData.metadata.ishandcuffed then return sleep end
+    sleep = 0
+    HandcuffActions()
+    if PlayerData.metadata.isdead then return sleep end
+    for _, anim in anim do
+        if IsEntityPlayingAnim(cache.ped, anim.dict, anim.anim, 3) then return sleep end
     end
+    lib.requestAnimDict("mp_arresting")
+    TaskPlayAnim(cache.ped, "mp_arresting", "idle", 8.0, -8, -1, cuffType, 0, false, false, false)
+
     return sleep
 end
 
@@ -392,9 +402,7 @@ end)
 
 -- Threads
 CreateThread(function()
-    local sleep
     while true do
-        sleep = HandcuffedEscorted()
-        Wait(sleep)
+        Wait(HandcuffedEscorted())
     end
 end)
