0bd249eb613097ded0a501f6c39cc0db2c2590ab
Tony <97451137+TonybynMp4@users.noreply.github.com>
2023-04-26 13:03:37 +0200

    Update anpr.lua

--- client/anpr.lua
+++ client/anpr.lua
@@ -14,25 +14,24 @@ local function SpeedRange(speed)
 end
 
 local function HandlespeedCam(speedCam, radar)
-	if cache.vehicle and cache.seat == -1 and GetVehicleClass(cache.vehicle) ~= 18 then
-		local plate = QBCore.Functions.GetPlate(cache.vehicle)
-		local speed = GetEntitySpeed(cache.vehicle) * (Config.MPH and 2.236936 or 3.6)
-		local OverLimit = speed - speedCam.speed
+	if not cache.vehicle or cache.seat ~= -1 or GetVehicleClass(cache.vehicle) == 18 then return end
+	local plate = QBCore.Functions.GetPlate(cache.vehicle)
+	local speed = GetEntitySpeed(cache.vehicle) * (Config.MPH and 2.236936 or 3.6)
+	local OverLimit = speed - speedCam.speed
 
-		QBCore.Functions.TriggerCallback('police:IsPlateFlagged', function(result)
- 			if not result then return end
-			local s1, s2 = GetStreetNameAtCoord(speedCam.coords.x, speedCam.coords.y, speedCam.coords.z)
-			local street = GetStreetNameFromHashKey(s1)
-			local street2 = GetStreetNameFromHashKey(s2)
-			if street2 then
-				street = street .. ' | ' .. street2
-			end
-			TriggerServerEvent("police:server:FlaggedPlateTriggered", radar, plate, street)
-		end, plate)
+	QBCore.Functions.TriggerCallback('police:IsPlateFlagged', function(result)
+ 		if not result then return end
+		local s1, s2 = GetStreetNameAtCoord(speedCam.coords.x, speedCam.coords.y, speedCam.coords.z)
+		local street = GetStreetNameFromHashKey(s1)
+		local street2 = GetStreetNameFromHashKey(s2)
+		if street2 then
+			street = street .. ' | ' .. street2
+		end
+		TriggerServerEvent("police:server:FlaggedPlateTriggered", radar, plate, street)
+	end, plate)
 
-		if not Config.SpeedFines or OverLimit < 0 then return end
-		SpeedRange(OverLimit)
-	end
+	if not Config.SpeedFines or OverLimit < 0 then return end
+	SpeedRange(OverLimit)
 end
 
 
@@ -44,9 +43,9 @@ CreateThread(function()
 			speed = value.speedlimit,
 		})
 	end
-	for _, v in pairs(SpeedCams) do
+	for k, v in pairs(SpeedCams) do
 		function v:onEnter()
-			HandlespeedCam(self, _)
+			HandlespeedCam(self, k)
 		end
 	end
 end)
