50bf83cfae40ff1f310e3dbd569ba65eb8674115
bicatcho11 <151503460+bicatcho11@users.noreply.github.com>
2024-02-21 08:36:28 +0300

    Moved some code to the configuration file

--- config/server.lua
+++ config/server.lua
@@ -2,4 +2,8 @@ return {
     licenseRank = 2, -- Grade needed to give out weapon licenses
     towPay = 500, -- Police tow paid
     lawyerPay = 500, -- Police lawyer paid
+    validLicense = { -- New valid licenses
+        driver = true,
+        weapon = true,
+    }
 }
