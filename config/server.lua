c8f6d18f49f8940740295059f6636c4254744ff5
bicatcho11 <151503460+bicatcho11@users.noreply.github.com>
2024-02-25 19:24:45 +0300

    Update server.lua

--- config/server.lua
+++ config/server.lua
@@ -2,7 +2,7 @@ return {
     licenseRank = 2, -- Grade needed to give out weapon licenses
     towPay = 500, -- Police tow paid
     lawyerPay = 500, -- Police lawyer paid
-    validLicense = { -- valid licenses
+    validLicenses = { -- valid licenses
         driver = true,
         weapon = true,
     },
