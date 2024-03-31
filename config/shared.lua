0159183e594e9bff2b6ccaf3cb93cb841f77cf89
bicatcho11 <151503460+bicatcho11@users.noreply.github.com>
2024-03-31 22:40:25 +0300

    Customize the blip in a better

--- config/shared.lua
+++ config/shared.lua
@@ -45,12 +45,12 @@ return {
         evidence = { -- Not currently used, use ox_inventory evidence system
         },
         stations = {
-            {label = 'Mission Row Police Station', coords = vec3(434.0, -983.0, 30.7)},
-            {label = 'Sandy Shores Police Station', coords = vec3(1853.4, 3684.5, 34.3)},
-            -- {label = 'Vinewood Police Station', coords = vec3(637.1, 1.6, 81.8)},
-            -- {label = 'Vespucci Police Station', coords = vec3(-1092.6, -808.1, 19.3)},
-            -- {label = 'Davis Police Station', coords = vec3(368.0, -1618.8, 29.3)},
-            -- {label = 'Paleto Bay Police Station', coords = vec3(-448.4, 6011.8, 31.7)},
+            {label = 'Mission Row Police Station', coords = vec3(434.0, -983.0, 30.7), sprite = 60, colour = 3, size = 0.8},
+            {label = 'Sandy Shores Police Station', coords = vec3(1853.4, 3684.5, 34.3), sprite = 60, colour = 29, size = 0.8},
+            -- {label = 'Vinewood Police Station', coords = vec3(637.1, 1.6, 81.8), sprite = 60, colour = 3, size = 0.8},
+            -- {label = 'Vespucci Police Station', coords = vec3(-1092.6, -808.1, 19.3), sprite = 60, colour = 3, size = 0.8},
+            -- {label = 'Davis Police Station', coords = vec3(368.0, -1618.8, 29.3), sprite = 60, colour = 3, size = 0.8},
+            -- {label = 'Paleto Bay Police Station', coords = vec3(-448.4, 6011.8, 31.7), sprite = 60, colour = 3, size = 0.8},
         },
     },
 
