091b83f8ec042f356743cdf83eef0fac4eacb541
Matthew <22198949+mafewtm@users.noreply.github.com>
2024-07-26 01:06:58 -0400

    feat(server/main): missing spawnVehicle callback
    * feat(server/main): missing spawnVehicle callback
    
    * fix: typo
    
    * fix: pass plate as props
    
    * fix: remove unused variable

---
+++ server/main.lua
@@ -0,0 +1,19 @@
+---@param source number
+---@param model string
+---@param spawn vector4
+lib.callback.register('qbx_police:server:spawnVehicle', function(source, model, spawn)
+    local ped = GetPlayerPed(source)
+    local plate = ('LSPD%s'):format(math.random(1000, 9999))
+    local netId, _ = qbx.spawnVehicle({
+        spawnSource = spawn,
+        model = model,
+        warp = ped,
+        props = {
+            plate = plate
+        }
+    })
+
+    exports.qbx_vehiclekeys:GiveKeys(source, plate)
+
+    return netId
+end)
