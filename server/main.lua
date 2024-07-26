e3042e43e7d0b312bf683b82d0045fcddfc56a39
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-07-26 01:04:36 -0400

    fix: pass plate as props

--- server/main.lua
+++ server/main.lua
@@ -3,15 +3,16 @@
 ---@param spawn vector4
 lib.callback.register('qbx_police:server:spawnVehicle', function(source, model, spawn)
     local ped = GetPlayerPed(source)
+    local plate = ('LSPD%s'):format(math.random(1000, 9999))
     local netId, veh = qbx.spawnVehicle({
         spawnSource = spawn,
         model = model,
-        warp = ped
+        warp = ped,
+        props = {
+            plate = plate
+        }
     })
 
-    local plate = ('LSPD%s'):format(math.random(1000, 9999))
-
-    SetVehicleNumberPlateText(veh, plate)
     exports.qbx_vehiclekeys:GiveKeys(source, plate)
 
     return netId
