2f29f3ccc3d27ddf698c97d52f3b0771cfc704d7
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-10-10 14:12:54 -0400

    feat: readding officer blips

--- config/server.lua
+++ config/server.lua
@@ -1,4 +1,6 @@
 return {
+    refreshRate = 1000, -- Time in ms between the refreshing of officer blips
+
     giveVehicleKeys = function(source, vehicle)
         return exports.qbx_vehiclekeys:GiveKeys(source, vehicle)
     end,
